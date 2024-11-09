4efc880122857bc3a6c81d653c3aff918e0f5039
carlocef <160526604+carlocef@users.noreply.github.com>
2024-11-09 15:43:12 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routerApi = require('./routes');
+const routerApi = require('./routes/index');
 const cors = require('cors');
 const {errorHandler, boomErrorHandler  } = require('./middlewares/errorHandler');
 const app = express();
